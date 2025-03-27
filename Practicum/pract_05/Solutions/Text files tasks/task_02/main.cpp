ac2fc43a65157e4a5e98d399eff2644a1091a451
Hristo Georgiev <Hrisge@abv.bg>
2025-03-27 14:26:56 +0200

    Tasks added for week 6

--- Practicum/pract_05/Solutions/Text files tasks/task_02/main.cpp
+++ Practicum/pract_05/Solutions/Text files tasks/task_02/main.cpp
@@ -15,36 +15,39 @@ int getMult(int a, int b, int c) {
     return a * b * c;
 }
 
-void writeToFile(int sum, int mult, std::ofstream& ofs) {
+void writeToFile() {
+    int a, b, c;
+    input(a, b, c);
+ 
+    int sum = getSum(a, b, c);
+    int mult = getMult(a, b, c);
+
+    std::ofstream ofs(FILE_NAME);
     if (!ofs.is_open()) {
         std::cout << "File could not open!" << std::endl;
         return;
     }
     ofs << sum << mult;
+
+    ofs.close();
 }
 
-int readFromFileAndGetResult(std::ifstream& ifs) {
+int readFromFileAndGetResult() {
+    std::ifstream ifs(FILE_NAME);
+    
     if (!ifs.is_open()) {
         std::cout << "File could not open!" << std::endl;
         return -1;
     }
+
     int sum, mult;
     ifs >> sum >> mult;
-    return sum - mult;
+
+    std::cout << sum - mult;
+
+    ifs.close();
 }
 
 int main() {
-    int a, b, c;
-    input(a, b, c);
-    std::ofstream ofs(FILE_NAME);
-    int sum = getSum(a, b, c);
-    int mult = getMult(a, b, c);
-
-    writeToFile(sum, mult, ofs);
-
-    std::ifstream ifs(FILE_NAME);
-    std::cout << readFromFileAndGetResult(ifs);
-
-    ofs.close();
-    ifs.close();
+ 
 }
